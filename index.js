4deb40c44d74c4caa70c2ad0be99952e9a561cb5
Santeri Einola <einola.santeri@gmail.com>
2021-04-12 20:30:32 +0300

    tehtävä 4.2 valmis

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://fullstack:fullstack@cluster0.blvny.mongodb.net/blogApp?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (req, res) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            res.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (req, res) => {
-    const blog = new Blog(req.body)
-
-    blog
-        .save()
-        .then(result => {
-            res.status(201).json(result)
-        })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
 })
